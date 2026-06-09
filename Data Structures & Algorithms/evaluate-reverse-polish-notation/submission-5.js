a08953aa1a5631b45953a0bc49c94bd59e8945d4
ckjn0411 <154370824+ckjn0411@users.noreply.github.com>
2026-06-10 01:32:15 +0700

    Add: evaluate-reverse-polish-notation - submission-5

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-5.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    
+    evalRPN(tokens) {
+        function calculate(num1, opString, num2) {
+            num1 = parseInt(num1)
+            num2 = parseInt(num2)
+            switch (opString) {
+                case '+': return num1 + num2;
+                case '-': return num2 - num1;
+                case '*': return num1 * num2;
+                case '/': return Math.trunc(num2 / num1);
+                default:  return "Invalid operator";
+            }
+        }
+
+        let stack = []
+        for(let c of tokens){
+            if(c.match("[0-9]")){
+                stack.push(c)
+                // console.log(c)
+            }else{
+                let n1 = stack.pop()
+                let n2 = stack.pop()
+                // swap(n1, n2)
+                console.log('n1 ' + n1)
+                console.log('n2 ' + n2)
+                console.log('c ' + c)
+                console.log('res ' + calculate(n1,c,n2))
+                stack.push(calculate(n1,c,n2))
+
+            }
+        }
+
+        return stack.pop()
+    }
+}
